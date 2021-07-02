7358902b3b1b28992f870ee3852a92f1b14ae12d
Daniel R. Vásquez Montes <dev.DanielR@gmail.com>
2021-07-02 01:21:47 -0500

    Corrected title

--- views/site/index.php
+++ views/site/index.php
@@ -1,8 +1,8 @@
+<?php
+  $this->title = "EasyUI - Dynamic Tabs";
+?>
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EasyUI - Dynamic Tabs</title>
   <link rel="stylesheet" href="easyui/themes/<?= $theme ?>/easyui.css"> 
   <link rel="stylesheet" href="easyui/themes/icon.css">
   <link rel="stylesheet" href="custom/index.css">
